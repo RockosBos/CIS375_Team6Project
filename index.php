701d9112506c0e41bb8af52a32dc413a3def2d27
Nicholas Kessey <nkessey@umich.edu>
2019-11-17 16:35:05 -0500

    Initial Setup Test
    Added in basic functionality  and linked CSS file.

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <link rel="stylesheet" type="text/css" href="./style.css">
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1> CIS375 Test Case Generator </h1>
+    <h3> Nick Kessey <br> Colin McLaughlin <br> Walker Bass <br> Ryan Way <br> </h3>
+
+  </body>
+</html>
