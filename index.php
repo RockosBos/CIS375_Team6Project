6cdb33545dd0977d2db71c7fa7fba1dc98acf9c4
Nicholas Kessey <nkessey@umich.edu>
2019-11-17 21:58:52 -0500

    Added Instructions Section
    Instructions section added as well as some basic CSS to test formatting

--- index.php
+++ index.php
@@ -7,7 +7,19 @@
   </head>
   <body>
     <h1> CIS375 Test Case Generator </h1>
-    <h3> Nick Kessey <br> Colin McLaughlin <br> Walker Bass <br> Ryan Way <br> </h3>
+    <h3> Created By: Nick Kessey - Colin McLaughlin - Walker Bass - Ryan Way</h3>
+    <hr>
+    Instructions:
+    <br>
+    <p id="OperationInstruction">
+        Enter the name of your operation here. This will be what the Generator will use to create test cases.
+    </p>
+    <p id="RelationshipInstruction">
+        Enter all of the Relationships the operation will have here. The connections across these relationships will generate the test cases.
+    </p>
+    <form action="php-test.php" method="get">
+        <input type="text" name="Input 1">
+    </form>
 
   </body>
 </html>
