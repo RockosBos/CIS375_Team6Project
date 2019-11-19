ff64f385e4a4462b2b42c107c06b4de5dd38dee7
Nicholas Kessey <nkessey@umich.edu>
2019-11-19 18:38:40 -0500

    Added test box and submit button as well as comments

---
+++ OperationClass.js
@@ -0,0 +1,8 @@
+class Operation{
+  constructor(id, name, relationships[]){
+    this.operationID = id;
+    this.name = name;
+    this.relationships[] = relationships[];
+  }
+  
+}
--- index.php
+++ index.php
@@ -11,15 +11,23 @@
     <hr>
     Instructions:
     <br>
+    <!-- This is description of the operation box, it will only appear once on the top left of the page-->
     <p id="OperationInstruction">
         Enter the name of your operation here. This will be what the Generator will use to create test cases.
     </p>
+    <!-- This is the description of the Relationships, it will be located on the top right of the screen. -->
     <p id="RelationshipInstruction">
         Enter all of the Relationships the operation will have here. The connections across these relationships will generate the test cases.
     </p>
-    <form action="php-test.php" method="get">
-        <input type="text" name="Input 1">
+    <form action="index.php" method="get">
+        <input type="text" name="Input1">
+        <input type="submit" text="Generate Test Cases">
     </form>
+
+        <?php
+          //this will be the beginning of the php for repeating and loading the operations and relationships into the operation array.
+            echo $_GET ["Input1"];
+         ?>
 
   </body>
 </html>
