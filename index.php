407e926966b2797fbe4dd9d3a4c5eb2d427565d3
Nicholas Kessey <nkessey@umich.edu>
2019-11-21 00:03:31 -0500

    Added text box and new frmatting
    dont worry about the images

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <h1> CIS375 Test Case Generator </h1>
     <h3> Created By: Nick Kessey - Colin McLaughlin - Walker Bass - Ryan Way</h3>
     <hr>
-    Instructions:
+    <div id="Instructions" >Instructions:</div>
     <br>
     <!-- This is description of the operation box, it will only appear once on the top left of the page-->
     <p id="OperationInstruction">
@@ -20,13 +20,17 @@
         Enter all of the Relationships the operation will have here. The connections across these relationships will generate the test cases.
     </p>
     <form action="index.php" method="get">
-        <input type="text" name="Input1">
-        <input type="submit" text="Generate Test Cases">
+      <div id="Inputs">
+        <input type="text" name="Input1" id="OperationInput">
+        <input type="text" name="Input2" id="RelationshipInput">
+      </div>
+      <input type="submit" text="Generate Test Cases" value="Generate Test Cases" id="TestCaseButton">
     </form>
     <script src="OperationClass.js"></script>
         <?php
           //this will be the beginning of the php for repeating and loading the operations and relationships into the operation array.
             echo $_GET ["Input1"];
+            echo $_GET ["Input2"];
          ?>
 
   </body>
